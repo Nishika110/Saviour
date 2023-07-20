1af84ba1144bc1f391cc43c60175d1def8a51661
Nishika <nishika.abhangi@gmail.com>
2023-07-20 19:47:21 +0530

    Basic frontend files set up

--- OneDrive/Desktop/Saviour/client/src/App.js
+++ OneDrive/Desktop/Saviour/client/src/App.js
@@ -1,10 +1,18 @@
 
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Home from "./pages/Home/index.js";
+import Login from "./pages/Login/index.js";
+import Register from "./pages/Register/index.js";
 
 function App() {
   return (
-    <div >
-      <h1>Saviour</h1>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login/" element={<Login />} />
+      <Route path="/register/" element={<Register />} />
+    </Routes>
+   </BrowserRouter>
   );
 }
 
