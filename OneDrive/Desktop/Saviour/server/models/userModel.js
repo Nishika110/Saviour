2fab6c24f9c14451701bc1bfcb6b304fd47e1014
Nishika <nishika.abhangi@gmail.com>
2023-07-23 11:51:41 +0530

    user model and api calls setup

---
+++ OneDrive/Desktop/Saviour/server/models/userModel.js
@@ -0,0 +1,76 @@
+const mongoose=require("mongoose");
+const userSchema=new mongoose.Schema({
+   
+    userType:{
+        type: String,
+        required: true,
+        enum: ['donor','organization','hospital','admin'],
+
+    },
+    //donor or admin
+    name:{
+        type:String,
+        required:function(){
+            if( this.userType==='admin'||this.userType==='donor'){
+                return true;
+            }
+            return false;
+        },
+    },
+    //hospital name
+    hospitalName:{
+        type:String,
+        required:function(){
+            if( this.userType==='hospital'){
+                return true;
+            }
+            return false;
+        },
+    },
+    organizationName:{
+        type:String,
+        required:function(){
+            if( this.userType==='organization'){
+                return true;
+            }
+            return false;
+        },
+    },
+    website:{
+        type:String,
+        required:function(){
+            if( this.userType==='organziation'||this.userType==='hospital'){
+                return true;
+            }
+            return false;
+        },
+       
+        },
+        address:{
+            type:String,
+            required:function(){
+                if( this.userType==='organziation'||this.userType==='hospital'){
+                    return true;
+                }
+                return false;
+            },
+           
+            },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        },
+    password:{
+        type:String,
+        required:true,
+       
+         },  
+        phone:{
+            type:String,
+            required:true,
+           
+         }         
+});
+
+module.exports=mongoose.model("users",userSchema);
